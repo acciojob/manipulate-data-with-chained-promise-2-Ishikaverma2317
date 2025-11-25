6dc1ab72622bd62445ee60f7ec4ac0ee96e6d951
Ishika Verma <ishikaverma2317@gmail.com>
2025-11-25 14:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+const output = document.getElementById("output");
+
+new Promise((resolve) => {
+  setTimeout(() => {
+    resolve([1, 2, 3, 4]);
+  }, 3000);
+})
+  .then((arr) => {
+    return new Promise((resolve) => {
+      const evens = arr.filter((num) => num % 2 === 0);
+      setTimeout(() => {
+        output.innerText = evens.join(",");
+        resolve(evens);
+      }, 1000);
+    });
+  })
+  .then((evens) => {
+    return new Promise((resolve) => {
+      const doubled = evens.map((num) => num * 2);
+      setTimeout(() => {
+        output.innerText = doubled.join(",");
+        resolve(doubled);
+      }, 2000);
+    });
+  });
